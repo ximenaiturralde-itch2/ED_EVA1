48f8627e5d945372a2938abed618db4c531b6527
ximenaiturralde-itch2 <L25550587@chihuahua2.tecnm.mx>
2026-09-03 19:39:12 -0600

    Add files via upload

---
+++ EVA1_3_STACKOVERFLOW/src/eva1_3_stackoverflow/EVA1_3_STACKOVERFLOW.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_stackoverflow;
+
+/**
+ *
+ * @author XIMENA ALEJANDRA ITURRALDE CONTRERAS
+ */
+public class EVA1_3_STACKOVERFLOW {
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        int i = 5;
+        double d = 5.5;
+        A();
+    }
+    
+}
