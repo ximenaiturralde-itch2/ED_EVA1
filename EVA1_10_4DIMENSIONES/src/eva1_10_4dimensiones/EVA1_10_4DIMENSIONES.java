bd78cbf468d5a69397b607e8c2e1cad30bde502d
ximenaiturralde-itch2 <L25550587@chihuahua2.tecnm.mx>
2026-09-14 16:50:29 -0600

    Add files via upload

---
+++ EVA1_10_4DIMENSIONES/src/eva1_10_4dimensiones/EVA1_10_4DIMENSIONES.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_4dimensiones;
+
+/**
+ *
+ * @author ca-co
+ */
+public class EVA1_10_4DIMENSIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][][][] arreglo = new int [2][2][2][2]; // almacena 16 enteros
+        for (int i = 0; i <arreglo.length; i++){//1er dimension
+            for (int j = 0; j < arreglo[i].length; j++){//2da
+                for (int k = 0;k < arreglo[i][j].length; k++){//3ra
+                    for(int l = 0; 1< arreglo[i][j][k].length; l++){//4ta
+                  System.out.println("["+ arreglo [i][j][k][l]+"]");
+              }  
+            }
+        }
+            
+        }
+    }
+   
+        int[][][][] arreglo = new int [2][2][2][2]; // almacena 16 enteros
+        for (int i = 0; i <arreglo.length; i++){//1er dimension
+            for (int j = 0; j < arreglo[i].length; j++){//2da
+                for (int k = 0;k < arreglo[i][j].length; k++){//3ra
+                    for(int l = 0; 1< arreglo[i][j][k].length; l++){//4ta
+                  System.out.println("["+ arreglo [i][j][k][l]+"]");
+              }  
+            }
+        }
+            
+        }
+    } 
